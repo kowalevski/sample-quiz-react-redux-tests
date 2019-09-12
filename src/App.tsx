013aefa013df908d4d751b73daca557fad6aea89
Maxim Kovalevsky <Maxim.Kovalevsky@nordigy.ru>
2019-09-12 07:32:52 +0300

    added typescript support and deleted unused files

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Simple Notes App</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
